ef81a1d943bce130aebf9d66c9049564c61f3aa8
Ivan Korobov <ikorobov.dev@gmail.com>
2025-01-25 12:52:40 +0100

    Initial Commit

---
+++ CryptoAssessment/CryptoAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAssessmentApp.swift
+//  CryptoAssessment
+//
+//  Created by ikorobov on 25.1.25..
+//
+
+import SwiftUI
+
+@main
+struct CryptoAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
